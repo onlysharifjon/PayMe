cfc63981af6642328b530a063d5a77c83ecf0f6e
SolijonovAsadbek <solijonovasadbek25111999@gmail.com>
2024-04-16 15:58:57 +0500

    16-aprel 15:58

--- paymeusers/admin.py
+++ paymeusers/admin.py
@@ -6,19 +6,19 @@ from .models import UzCard, HumoCard, VisaGold, VisaClassic
 
 @admin.register(UzCard)
 class UzCardAdmin(admin.ModelAdmin):
-    list_display = ['__all__']
+    list_display = ['id', 'number', 'owner_name', 'created', 'expired']
 
-
-@admin.register(HumoCard)
-class HumoCardAdmin(admin.ModelAdmin):
-    list_display = ['__all__']
-
-
-@admin.register(VisaGold)
-class VisaGoldAdmin(admin.ModelAdmin):
-    list_display = ['__all__']
-
-
-@admin.register(VisaClassic)
-class VisaClassicAdmin(admin.ModelAdmin):
-    list_display = ['__all__']
+#
+# @admin.register(HumoCard)
+# class HumoCardAdmin(admin.ModelAdmin):
+#     pass
+#
+#
+# @admin.register(VisaGold)
+# class VisaGoldAdmin(admin.ModelAdmin):
+#     pass
+#
+#
+# @admin.register(VisaClassic)
+# class VisaClassicAdmin(admin.ModelAdmin):
+#     pass
--- paymeusers/models.py
+++ paymeusers/models.py
@@ -16,26 +16,20 @@ class BaseCard(models.Model):
     ]
     card_type = models.CharField(max_length=50, choices=CARD_TYPES)
     number = models.CharField(max_length=16, unique=True)
-    pin_code = models.PositiveSmallIntegerField(validators=[MinValueValidator(4), MaxValueValidator(4)])
+    pin_code = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(9999)])
     owner_name = models.ForeignKey(User, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
 
     created = models.DateTimeField(auto_now_add=True)
-    expired = models.DateTimeField(default=lambda: timezone.now() + timedelta(days=365 * 4), editable=False)
+    expired = models.DateTimeField(default=timezone.now() + timedelta(days=365 * 4), editable=False)
 
     class Meta:
         abstract = True
-    # expired_date = models.DateTimeField(null=True, blank=True)
-    # def save(self, *args, **kwargs):
-    #     if not self.expired_date:  # Agar expired_date to'ldirilmagan bo'lsa
-    #         # created sana + 4 yilni hisoblash
-    #         self.expired_date = self.created + timedelta(days=365 * 4)  # 4 yil = 4 * 365 kun
-    #
-    #     super().save(*args, **kwargs)  # Ota-class save metodini chaqirish
 
 
 class UzCard(BaseCard):
-    pass
+    def  __str__(self):
+        return self.number
 
 
 class HumoCard(BaseCard):
@@ -48,7 +42,6 @@ class VisaGold(BaseCard):
 
 class VisaClassic(BaseCard):
     pass
-
 
 # class TolovlarTarixi(models.Model):
 #     time = models.DateTimeField(auto_now_add=True)
