137dd09a6c9c02cb18b562b91ed67838145404a9
Sharifjon Mo'minov <sharif@Sharifjons-MacBook-Air.local>
2024-04-20 15:07:49 +0500

    commit

--- Config/settings.py
+++ Config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+# omon oken
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
